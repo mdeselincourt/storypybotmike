7086ef6dedb53e314a7ab1f31caa824e9bb05604
Mike de Selincourt <m.de.selincourt@googlemail.com>
2018-11-25 11:13:21 +0000

    Builds tweet from manuscript tree of sentences

--- storypybotmike.py
+++ storypybotmike.py
@@ -1,10 +1,11 @@
 import twitter # python-twitter library
 import json
 import logging
-import boto3 # Boto (an amazon river dolphin!) is the AWS Python library
-import urllib.request # To be able to fetch data over http
+import boto3 # Boto (a species of Amazon river dolphin!) is the AWS Python library
+import urllib.request # To be able to fetch data over http if assets are online not deployed in the app
 import random
 import os # Helps me import files
+from objdict import ObjDict # Helps me treat Python as if it's JavaScript haha
 
 okToTweet = False # If in doubt, don't tweet!
 
@@ -48,30 +49,83 @@ def main():
 	with open('dariusk/animals.common.json') as animalsCommonCorpus:
 		animalsCommonCorpus = json.loads(animalsCommonCorpus.read()) # Deserialise the JSON Object into its python equivalent, a <dict>
 
-	class Story:
+	class Entity:
 		pass
-		
-	class Protagonist:
-		pass
-		
-	story = Story()
-	story.protagonist = Protagonist()
-	story.protagonist.name = random.choice(firstNamesCorpus["firstNames"])
-	story.protagonist.identity = (random.choice(animalsCommonCorpus["animals"])).capitalize()
-	story.protagonist.startingNaturalMood = random.choice(moodsCorpus["deplorableMoods"])
-	story.protagonist.endingNaturalMood = random.choice(moodsCorpus["admirableMoods"])
 	
-	p = story.protagonist
+	narrative = Entity()
+	narrative.protagonist = Entity()
+	narrative.protagonist.name = random.choice(firstNamesCorpus["firstNames"])
+	narrative.protagonist.identity = (random.choice(animalsCommonCorpus["animals"])).capitalize()
+	narrative.protagonist.startingNaturalMood = random.choice(moodsCorpus["deplorableMoods"])
+	narrative.protagonist.endingNaturalMood = random.choice(moodsCorpus["admirableMoods"])
+	narrative.mentor = Entity()
+	narrative.mentor.name = random.choice(firstNamesCorpus["firstNames"])
+	narrative.mentor.identity = random.choice(animalsCommonCorpus["animals"]).capitalize()
+	narrative.mentor.naturalMood = random.choice(moodsCorpus["admirableMoods"])
 	
-	output = "Here's a folk story. " + p.name + " the " + p.identity + " usually felt " + p.startingNaturalMood + ". But after an adventure, " + p.name + " felt " + p.endingNaturalMood + " most days."
+	"""
+	{
+		"Virtues": {
+			"Cardinal": ["Prudence", "Courage", "Temperance", "Justice"],
+			"HEXACO": ["Honesty", "Emotionality", "Openness to experience", "Conscientiousness", "Extraversion", "Agreeableness", "Un-neuroticism"]
+			"Theological": ["Faith", "Hope", "Charity"],
+			"Leadership": ["Focus", "Grit"]
+		}
+	}
+	"""
+	
+	p = narrative.protagonist
+	m = narrative.mentor
+	
+	# Write the story in manuscript form i.e. broken down into sentences short enough to tweet.
+	manuscript = ObjDict()
+	manuscript.acts = []
+
+	# Act 0 
+	manuscript.acts.append(["Here's a folk story."])
+	
+	# Act 1
+	manuscript.acts.append([]) # Add act 1
+	manuscript.acts[1].append(p.name + " the " + p.identity + " usually felt " + p.startingNaturalMood + ".")
+	
+	# Act 2
+	manuscript.acts.append([]) # Add act 2
+	manuscript.acts[2].append("But one day " + p.name + " met " + m.name + " the " + m.identity + ".")
+	
+	# Act 3
+	manuscript.acts.append([])
+	manuscript.acts[3].append("They went on an adventure, after which " + p.name + " felt " + p.endingNaturalMood + " most days.")
 		
-	output = "My bot says: " + output
+	logger.info("manuscriptJson is\n" + json.dumps(manuscript))
+		
+	lenBudget = 266
+	
+	storySnippet = ""
+	
+	for act in manuscript.acts:
+		for line in act:
+			if (len(line) == 0):
+				logger.info("Skipping an empty line")
+				pass
+			elif (len(line) < lenBudget):
+				storySnippet += " " + line
+				line = ""
+			else:
+				break; break;
+		
+	output = "My bot says: " + storySnippet
 	
 	logger.info(output)
 	
-	if (len(output) > 280):
+	logger.info(str(len(output)) + " of 280 characters")
+	
+	if len(output) > 280:
 		okToTweet = False
 		logger.error("Intended tweet is too long")
+		
+	if len(output) < 13:
+		okToTweet = False
+		logger.error("Intended tweet is concerningly short")
 		
 	################# ACT ON TWITTER #################
 		
@@ -97,7 +151,7 @@ def main():
 		status = python_twitter.PostUpdate(output)
 		logger.info("Tweeted; status = " + str(status))
 	else:
-		logger.info("Not OK to tweet");
+		logger.warning("Not OK to tweet");
 	
 # Configure this in lambda as the handler that Lambda will invoke
 def lambda_handler(_event_json, _context):
