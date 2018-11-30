e00c0e800a70750760d882c81226123b2422de81
Mike de Selincourt <m.de.selincourt@googlemail.com>
2018-11-30 20:19:18 +0000

    Multi-tweet stories work in dev not prof

--- storypybotmike.py
+++ storypybotmike.py
@@ -7,6 +7,7 @@ import random
 import os # Helps me import files
 from objdict import ObjDict # Helps me treat Python as if it's JavaScript haha
 from botocore.exceptions import ClientError
+import sys
 
 # Don't forget to install these with pip -t . so they go in the repo and can be deployed to AWS
 
@@ -35,62 +36,60 @@ def get_poem_tweets():
 
 # Intended for either of two entry points - last line or lambda_handler()
 def main(environment):
-	logger.info("main() running in " + environment + " environment")
-
+	logger.info(" ------- main() running in " + environment + " environment -------")
+	## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+	
 	okToTweet = False; # If in doubt, don't tweet.
 
-	################# PERFORM LOGIC ##################
-		
-	#with urllib.request.urlopen("https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/moods.json") as url:
+	# S3 not in use (it's not always-free-tier)
+	# s3 = boto3.resource('s3') # Boto will automatically get credentials from the filesystem (running locally) or the function's user account (lambda)	
+	# s3ManuscriptOrNone = getManuscriptOrNoneFromS3(s3)
+	# logger.info("s3 manu = " + str(s3ManuscriptOrNone))
 	
-	s3 = boto3.resource('s3') # Boto will automatically get credentials from the filesystem (running locally) or the function's user account (lambda)	
-
-	logger.info(str(checkS3ForManuscript(s3)))
-	
-	## Writing new story
-	
-	corpora = loadCorpora()
-	
-	narrative = buildNarrative(corpora)
-	
-	manuscript = loadOrWriteManuscript(narrative, corpora, s3)
-	
-	# saveManuscript(manuscript)
-	
-	output = chooseOutput(manuscript)
-		
-	logger.info(output)
-	
-	### To Tweet or not to Tweet
-	
-	## Prerequisites
-	
-	# Turn tweeting ON if we're in the cloud
-	if (environment == "Lambda Production"):
-		okToTweet = True
-	else:
-		logger.warning("Environment is not Lambda Production. Not activating tweeting.")
-	
-	## Validation
-	
-	# Turn tweeting back OFF if the post is unacceptable
-	logger.info(str(len(output)) + " of 280 characters")
-	
-	if len(output) > 280:
-		okToTweet = False
-		logger.error("Intended tweet is too long, deactivating tweeting")
-		
-	if len(output) < 13:
-		okToTweet = False
-		logger.error("Intended tweet is concerningly short, deactivating tweeting")
-		
-	################# ACT ON TWITTER #################
-
-	if (okToTweet):
+	# Get Manuscript from dynamo (if there is one)															
+	dynamodb = boto3.resource('dynamodb', region_name='us-east-1')											
+	dynamoManuscriptOrNone = getManuscriptOrNoneFromDynamo(dynamodb)										
+	logger.info("fetched dynamo manuscript = " + str(dynamoManuscriptOrNone))								
+																											
+	if dynamoManuscriptOrNone == None:																		
+		# Write a new story before proceeding																
+		corpora = loadCorpora()																				
+																											
+		narrative = buildNarrative(corpora)																	
+																											
+		manuscript = createManuscript(narrative, corpora)													
+	else:																									
+		manuscript = dynamoManuscriptOrNone																	
+																											
+	# Tweet what we can then update the DB																	
+	outputAndRemainer = chooseOutputAndRemainer(manuscript)													
+																											
+	output = outputAndRemainer["output"]																	
+	remainer = outputAndRemainer["remainer"]															
+																											
+	logger.info("output = " + str(output))																	
+	logger.info("remainer = " + str(remainer))																
+																											
+	okToTweet = isOkToTweet(environment, output)															
+																										
+	if (okToTweet):																							
+		logger.warning("Tweeting")
 		tweet(output)
+	else:																									
+		logger.info("Not invoking tweet()") 																
+		
+	if (len(remainer) > 0):
+		logger.info("Saving remainer")
+		
+		newManuscript = {}
+		newManuscript["previous"] = manuscript["previous"] + 1
+		newManuscript["text"] = remainer
+		
+		saveManuscriptToDynamoDB(newManuscript, dynamodb)
 	else:
-		logger.warning("Skipping tweeting step");
-	#
+		logger.info("No remainer; deleting manuscript!")
+		deleteManuscriptFromDynamoDB(dynamodb)
+		
 #
 	
 ## Load Corpora ##
@@ -116,24 +115,7 @@ def loadCorpora():
 	return loadedCorpora
 #
 	
-## Build Narrative ##
-	"""
-	{
-		"Virtues": {
-			"Cardinal": ["Prudence", "Courage", "Temperance", "Justice"],
-			"HEXACO": ["Honesty", "Emotionality", "Openness to experience", "Conscientiousness", "Extraversion", "Agreeableness", "Un-neuroticism"]
-			"Theological": ["Faith", "Hope", "Charity"],
-			"Leadership": ["Focus", "Grit"]
-		}
-	}
-	"""
 def buildNarrative(corpora):
-	
-	#superVirtues = ["active", "confident", "kind", "positive", "wise"]
-	#superVices = ["selfish", "cowardly", "passive", "despairing", "hysterical"]
-	#heroisms = ["self-sacrificial", "brave", "tireless", "undespairing", "unflappable"]
-	#villainies = ["seductive", "fearless", "relentless", "fanatical", "calculating"]
-	#indulgences	= ["greedy", "cowardly", "slothful", "bi-polar", "irrational"]
 		
 	logger.info("buildNarrative() running...")
 	n = ObjDict() # Narrative
@@ -176,48 +158,47 @@ def buildNarrative(corpora):
 	logger.info("str(narrative) = " + str(n))
 	
 	return n;
-#
 	
-def checkS3ForManuscript(s3):
+def getManuscriptOrNoneFromS3(s3):
 	try:
-		getAttempt = s3.Object('storypybotmike', "Manuscript.json").get()
-		return True
-	except ClientError as e:
-		if e.response['Error']['Code'] == "NoSuchKey":
-			return False
+		response = s3.Object('storypybotmike', "Manuscript.json").get()
+		return json.loads(response['Body'].read().decode('utf-8'))
+	except ClientError as ce:
+		if ce.response['Error']['Code'] == "NoSuchKey":
+			return None
 		else:
-			logger.error("Error code is " + e.response['Error']['Code'])
+			raise
 	except Exception as ee:
 		logger.error("Unexpected exception: " + str(ee))
 	
-def loadOrWriteManuscript(narrative, corpora, s3):
-	logger.info("loadOrWriteManuscript() running...")
+def getManuscriptOrNoneFromDynamo(dynamodb):
+	logger.info("getManuscriptOrNoneFromDynamo() running...")
 	
-	## Check for a manuscript already in S3
-	
-	manuscriptFound = False
-	
-	## End up with either True and a getAttempt, or just False
 	try:
-		getAttempt = s3.Object('storypybotmike', "Manuscript.json").get()
-		logger.info("getAttempt is " + str(getAttempt))
-		manuscriptFound = True
-	except ClientError as e:
-		if e.response['Error']['Code'] == "NoSuchKey":
-			logger.info("File not found")
-		else:
-			logger.error("Error code is " + e.response['Error']['Code'])
-	except Exception as ee:
-		logger.error("WTF " + str(ee))
+		table = dynamodb.Table('storypybotmike')
 		
-	#logger.info("manuscriptFound = " + str(manuscriptFound)) 
+		response = table.get_item(
+			Key={
+				'myPartitionKey': 'Manuscript'
+			}
+		)
+		
+		try:
+			#logger.info("Response['Item'] = " + str(response["Item"]))
+			manuscript = json.loads(response['Item']['myJson'])
+		except KeyError:
+			#logger.info("Expected exception: KeyError => no stored manuscript")
+			return None
+		
+		return manuscript
 	
-	if manuscriptFound:
-		logger.info("Loading manuscript from S3")
-		return json.loads(getAttempt['Body'].read().decode('utf-8'))
-	else:
-		logger.info("Writing a new manuscript")
-		pass
+	except Exception as ee:
+		logger.error("Unexpected exception accessing dynamoDB!")
+		raise
+
+def createManuscript(narrative, corpora):
+	
+	logger.info("writeManuscript() running...")	
 	
 	## Prepare to write manuscript 
 	
@@ -232,66 +213,147 @@ def loadOrWriteManuscript(narrative, corpora, s3):
 	v.vice = corpora["superVirtues"][p.lackingVirtueKey]["indulgence"]	
 	
 	# Write the story in manuscript form i.e. broken down into sentences short enough to tweet.
-	manuscript = []
-
+	manuscript = {}
+	manuscript["previous"] = 0
+	manuscript["text"] = []
+	
 	# Act 0 
-	manuscript.append("Here's another folk story.")
+	
+	manuscript["text"].append("Here's another folk story.")
 	
 	# Act 1
-	manuscript.append(p.name + " was " + aan(p.virtue) + " but " + p.vice + " " + p.identity.title() + ".")
-	manuscript.append("Like all the animals, " + p.name + " lived under the tyranny of " + v.name + " the " + v.virtue.capitalize() + " " + v.identity.title() + ".")
+	manuscript["text"].append(p.name + " was " + aan(p.virtue) + " but " + p.vice + " " + p.identity.title() + ".")
+	manuscript["text"].append("Like all the animals, " + p.name + " lived under the tyranny of " + v.name + " the " + v.virtue.capitalize() + " " + v.identity.title() + ".")
 	
 	# Act 2
-	manuscript.append("One day, " + m.name + " the " + m.identity.title() + " asked for " + p.name + "'s help to rid them of " + v.name + ".")
-	manuscript.append(p.name + " agreed, and together they learned that " + v.name + " was prone to being " + v.vice + "." )
+	manuscript["text"].append("One day, " + m.name + " the " + m.identity.title() + " asked for " + p.name + "'s help to rid them of " + v.name + ".")
+	manuscript["text"].append(p.name + " agreed, and together they learned that " + v.name + " was prone to being " + v.vice + "." )
 	
 	# Act 3
 	
 	if (narrative.form == "heroic"):
-		manuscript.append("By the time they finally met, " + p.name + " had learned to be more " + p.lackingVirtueKey + ", and played on " + v.name + "'s " + v.vice + " side.")
-		manuscript.append(v.name + " was vanquished, and the animals lived happily ever after.")
+		manuscript["text"].append("By the time they finally met, " + p.name + " had learned to be more " + p.lackingVirtueKey + ", and played on " + v.name + "'s " + v.vice + " side.")
+		manuscript["text"].append(v.name + " was vanquished, and the animals lived happily ever after.")
 	elif (narrative.form == "tragic"):
-		manuscript.append("When they finally met, the " + v.identity + " " + v.name + " was feeling particularly " + v.virtue + ", and " + p.name + " could not help but be " + p.vice + ".")
-		manuscript.append(p.name + " was defeated, and " + m.name + " fled to seek a true hero. The end... or is it?")
+		manuscript["text"].append("When they finally met, the " + v.identity + " " + v.name + " was feeling particularly " + v.virtue + ", and " + p.name + " could not help but be " + p.vice + ".")
+		manuscript["text"].append(p.name + " was defeated, and " + m.name + " fled to seek a true hero. The end... or is it?")
 	else:
 		logger.error("No third act!")
-		manusript.append("... the end, I guess?")
+		manusript.text.append("... the end, I guess?!")
 		
 	#logger.info("manuscript json is:\n" + json.dumps(manuscript))
 	
 	return manuscript
 #
 
-def saveManuscript(manuscript):
-	logger.info("saveManuscript() running, str(manu..) = " + str(manuscript))
+def saveManuscriptToS3(manuscript, s3):
+	logger.info("saveManuscriptToS3() running...")
 			
 	try:
 		s3.Object('storypybotmike', "Manuscript.json").put(
 			Body=(bytes(json.dumps(manuscript, indent=2).encode('UTF-8')))
 		)
+		logger.info("Manuscript written to S3")
 	except:
 		logger.error("Exception while trying to write to s3 :(")
 	
-def chooseOutput(manuscript):
+def saveManuscriptToDynamoDB(manuscript, dynamodb):
+	logger.info("saveManuscriptToDynamoDB() running...")
+	
+	try:
+		table = dynamodb.Table('storypybotmike')
+		
+		response = table.put_item(
+			Item={
+				'myPartitionKey': 'Manuscript',
+				'myJson': json.dumps(manuscript)
+			}
+		)
+
+	except Exception as ee:
+		logger.error("Unexpected exception " + str(ee))
+		raise
+		
+	## ## ## ## ## ## ## ## ! ! ! ! ! ! ! !
+	
+def deleteManuscriptFromDynamoDB(dynamodb):
+	logger.info("deleteManuscriptFromDynamoDB() running...")
+	
+	try:
+		table = dynamodb.Table('storypybotmike')
+	
+		response = table.delete_item(Key={'myPartitionKey': 'Manuscript'})
+	except Exception as ee:
+		logger.error("Unexpected exception " + str(ee))
+		raise
+
+def chooseOutputAndRemainer(manuscript):
 	logger.info("chooseOutput() running...")
-	lenBudget = 266
+	lenBudget = 251
+	linesUnoutput = len(manuscript["text"])
 	
 	storySnippet = ""
+	remainer = []
 	
-	for line in manuscript:
+	logger.info("Composing output with " + str(linesUnoutput) + " lines remaining")
+	
+	for line in manuscript["text"]:
 		if (len(line) == 0):
-			logger.info("Skipping an empty line") # On assumption that we will empty tweeted lines and store what's left
-			pass
+			logger.info("Skipping an empty line")
 		elif (len(line) < lenBudget):
+			# Add line to selection of story
 			storySnippet += " " + line
-			line = ""
+			linesUnoutput += -1
 		else:
-			break; break;
+			# These lines are remainer
+			remainer.append(line)
+		# Whichever it was, update remaining budget
+		lenBudget = lenBudget - len(line)
+		
+	logger.info("Finishing tweet off, with " + str(linesUnoutput) + " lines remaining")
 		
 	output = "Bot here!" + storySnippet
 	
-	return output
+	if (linesUnoutput > 0):
+		output = output + " " + str(manuscript["previous"] + 1) + "/..."
+	else:
+		output = output + " " + str(manuscript["previous"] + 1) + "/" + str(manuscript["previous"] + 1)
+	
+	outputAndRemainer = {}
+	outputAndRemainer["output"] = output
+	outputAndRemainer["remainer"] = remainer
+	
+	return outputAndRemainer
 #
+
+def isOkToTweet(environment, output):
+	
+	logger.info("isOkToTweet() running...")
+	
+	ok = False
+	
+	## Prerequisites (False->True)
+	
+	# Turn tweeting ON if we're in the cloud
+	if (environment == "Lambda Production"):
+		ok = True
+	else:
+		logger.warning("Environment is not Lambda Production. Not activating tweeting.")
+	
+	## Validation (True->False)
+	
+	# Turn tweeting back OFF if the post is unacceptable
+	logger.info(str(len(output)) + " of 280 characters")
+	
+	if len(output) > 280:
+		ok = False
+		logger.error("Intended tweet is too long, deactivating tweeting")
+		
+	if len(output) < 13:
+		ok = False
+		logger.error("Intended tweet is concerningly short, deactivating tweeting")
+		
+	return ok
 
 def tweet(output):
 	logger.info("tweet() running...")
@@ -337,7 +399,7 @@ def aan(s):
     "detail": {}
 }
 """
-# Here's my new test event
+# Here's my custom test event
 """
 {
   "version": "0",
@@ -378,9 +440,9 @@ def lambda_handler(event, _context):
 #
 	
 # Detect 'standalone execution' and run main() if so
-logger.info("Checking for __main__ execution")
+# logger.info("Checking for __main__ execution")
 if __name__ == "__main__":
-	logger.info("__name__ is __main__")
+	#logger.info("__name__ is __main__")
 	main("Local")
 	logger.info("End of local execution")
 # This has to be the last thing...
